3cc17d85f4751c2d3bea243799a10cb936c289b0
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2024-05-29 11:20:04 +0300

    Initial commit

---
+++ 7 kyu/(7 kyu) Reverse words/(7 kyu) Reverse words.java
@@ -0,0 +1,18 @@
+public class Reverse {
+        System.out.println(reverseWords("Hi! I'm Eldar")); // !iH m'I radlE
+}
+
+public static String reverseWords(final String original)
+{
+    String[] word = original.split(" ");
+    if (word.length == 0)
+        return original;
+
+    int i = 0;
+    for(String string : word){
+        word[i] = new StringBuilder(string).reverse().toString();
+        i++;
+    }
+    return String.join(" ", word);
+}
+}
