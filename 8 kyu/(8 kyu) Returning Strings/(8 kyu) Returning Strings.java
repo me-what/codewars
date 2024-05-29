bcc7ce996969972ab84594934ae5f9e3002013b6
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2024-05-29 12:15:58 +0300

    5

---
+++ 8 kyu/(8 kyu) Returning Strings/(8 kyu) Returning Strings.java
@@ -0,0 +1,83 @@
+// 1
+
+public class Kata
+{
+    public static String greet(String name)
+    {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
+
+// 2
+public class Kata
+{
+    public static String greet(String name)
+    {
+        return String.format("Hello, %s how are you doing today?", name);
+    }
+}
+
+// 3
+public class Kata {
+    public static String greet(String name) {
+        if (name == null || name.length() < 1) {
+            return "Invalid input.";
+        }
+        return String.format("Hello, %s how are you doing today?", name);
+    }
+}
+
+// 4
+public class Kata
+{
+    public static String greet(String name)
+    {
+        return String.join(" ", new String[]{
+                "Hello,", name,
+                "how",
+                "are",
+                "you doing",
+                "today?"
+        });
+    }
+}
+
+// 4
+import java.util.*;
+public class Kata
+{
+    private static final String REPLACE = "replace";
+
+    public static String greet(String name)
+    {
+        StringBuilder sb = new StringBuilder();
+        List<String> characters = Arrays.asList(
+                "H", "e", "l", "l", "o", ",", " ", REPLACE,
+                " ", "h", "o", "w", " ", "a", "r", "e", " ",
+                "y", "o", "u", " ", "d", "o", "i", "n",
+                "g", " ", "t", "o", "d", "a", "y", "?"
+        );
+
+        for(String character : characters) {
+            if(Utils.equals(character, REPLACE)) {
+                character = name;
+            }
+            sb.append(character);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Utility class
+     *
+     */
+    private static class Utils {
+        private void Utils() {
+            throw new AssertionError("Instantiating utility class");
+        }
+
+        public static boolean equals(String char1, String char2) {
+            return char1 == char2;
+        }
+    }
+}
