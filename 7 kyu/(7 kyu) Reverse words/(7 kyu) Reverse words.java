fca4446ed1bb98d08cef679a52831cb4289a45ab
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2024-05-31 00:24:34 +0300

    5

--- 7 kyu/(7 kyu) Reverse words/(7 kyu) Reverse words.java
+++ 7 kyu/(7 kyu) Reverse words/(7 kyu) Reverse words.java
@@ -1,6 +1,27 @@
+// 1 Plain solution
+import java.*;
+
+public class Kata {
+    public static String reverseWords(final String original) {
+        String[] array = original.split(" ");
+        if (array.length == 0) {
+            return original;
+        }
+
+        List<String> res = new ArrayList<String>();
+        for (String w : array) {
+            StringBuilder r = new StringBuilder();
+            r.append(w);
+            r = r.reverse();
+            res.add(r.toString());
+        }
+        return String.join(" ", res);
+    }
+}
+
+
 // 2 Optimized solution
 import java.lang.StringBuilder;
-
 public class Reverse {
         System.out.println(reverseWords("Hi! I'm Eldar")); // !iH m'I radlE
 }
@@ -18,4 +39,17 @@ public static String reverseWords(final String original)
     }
     return String.join(" ", word);
 }
+}
+
+
+// 3 Clever solution
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Kata {
+    public static String reverseWords(final String original) {
+        return Arrays.stream(original.split("(?<=\\s)|(?=\\s+)"))
+                .map(str -> new StringBuilder(str).reverse().toString())
+                .collect(Collectors.joining());
+    }
 }
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,9 +1,7 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(convert(true));
-    }
 
-    public static String convert(boolean b) {
-        return String.valueOf(b);
     }
 }
