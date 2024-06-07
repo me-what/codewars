f607a4cff4496db9c50efb11dd45a86e6a0bfe7b
me-what <eldar.979751@gmail.com>
2024-06-08 00:25:56 +0300

    5

--- 6 kyu/(6 kyu) Stop gninnipS My sdroW!/(6 kyu) Stop gninnipS My sdroW!.java
+++ 6 kyu/(6 kyu) Stop gninnipS My sdroW!/(6 kyu) Stop gninnipS My sdroW!.java
@@ -45,3 +45,16 @@ public class SpinWords {
     return String.join(" ",words);
   }
 }
+
+
+// 3
+import java.util.stream.*;
+import java.util.Arrays;
+
+public class SpinWords {
+  public String spinWords(String sentence) {
+    return Arrays.stream(sentence.split(" "))
+            .map(i -> i.length() > 4 ? new StringBuilder(i).reverse().toString() : i)
+            .collect(Collectors.joining(" "));
+  }
+}
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,9 @@
-
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
+
     }
     }
