790f83a08c449239af34cd574c769e87f3d30e5e
me-what <eldar.979751@gmail.com>
2024-06-07 14:18:37 +0300

    5

---
+++ 6 kyu/(6 kyu) Stop gninnipS My sdroW!/(6 kyu) Stop gninnipS My sdroW!.java
@@ -0,0 +1,20 @@
+public class SpinWords {
+
+  public String spinWords(String sentence) {
+    String[] word = sentence.split(" ");
+    if (word.length == 0)
+      return sentence;
+
+    int i=0;
+    for (String string : word) {
+      if (word[i].length() >= 5) {
+        word[i] = new StringBuilder(string).reverse().toString();
+        i++;
+      } else {
+        word[i] = new StringBuilder(string).toString();
+        i++;
+      }
+    }
+    return String.join(" ", word);
+  }
+}
