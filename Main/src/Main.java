4146b57e1edded7b65c244fa66a68827990771c8
me-what <eldar.979751@gmail.com>
2024-06-11 17:58:16 +0300

    5

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     }
 
     public static boolean check(String sentence){
+
         return sentence.chars().map(Character::toLowerCase).filter(Character::isAlphabetic).distinct().count() == 26;
     }
     }
