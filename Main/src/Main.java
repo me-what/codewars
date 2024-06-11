362eff7ba1e039d86e7ed5f2032f2e5d342d0f20
me-what <eldar.979751@gmail.com>
2024-06-11 13:19:10 +0300

    5

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,12 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(check("The quick brown fox jumps over the lazy dog"));
-
     }
 
     public static boolean check(String sentence){
