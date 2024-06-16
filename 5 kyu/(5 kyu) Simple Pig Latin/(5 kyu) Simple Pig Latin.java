c1f74be44c2b6c4377bf548fe651cc0708a9a47a
me-what <eldar.979751@gmail.com>
2024-06-16 23:42:53 +0300

    5

--- 5 kyu/(5 kyu) Simple Pig Latin/(5 kyu) Simple Pig Latin.java
+++ 5 kyu/(5 kyu) Simple Pig Latin/(5 kyu) Simple Pig Latin.java
@@ -1,15 +1,15 @@
+// 1
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(pigIt("Pig latin is cool"));
-    }
-
+public class PigLatin {
     public static String pigIt(String str) {
-        return Arrays.stream(str.split("(?<=\\s)|(?=\\s+)")).map(word ->
+        return Arrays.stream(str.split("(?<=\\s+)|(?=\\s+)")).map(word ->
                 word.matches("[\\s\\p{Punct}]+") ? word
                         : word.substring(1) + word.charAt(0) + "ay"
         ).collect(Collectors.joining(""));
     }
-    }
+}
+
+
+// 2
